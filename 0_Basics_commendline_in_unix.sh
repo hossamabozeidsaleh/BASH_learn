dec982b20cc5218284a1cfc824ccd2953a0bf4ac
Hossam <125459729+hossamabozeidsaleh@users.noreply.github.com>
2024-05-21 20:14:30 +0300

    Update 0_Basics_commendline_in_unix.sh

--- 0_Basics_commendline_in_unix.sh
+++ 0_Basics_commendline_in_unix.sh
@@ -1,7 +1,7 @@
-echo "Hello studnt :)
-my name is : Hossam Abozeid Saleh 
-I'am a software engnering 
-i creat in the repo becose i leran now"
-echo "write by next ? yes/no "
+echo "Hello students :)
+My name is : Hossam Abozeid Saleh 
+I am a software engineer 
+I created this repo for the purpose of psychological education, and I hope it will benefit others"
+echo "All you want next ? yes/no "
 read x 
 
