c4ba467b2dce41cd8324f518ff6f65241979d689
hossam abozeid <hossamabozeidsaleh@gmail.com>
2024-05-21 13:36:54 -0400

    commit

--- 0_Basics_commendline_in_unix.sh
+++ 0_Basics_commendline_in_unix.sh
@@ -4,4 +4,19 @@ I am a software engineer
 I created this repo for the purpose of psychological education, and I hope it will benefit others"
 echo "All you want next ? yes/no "
 read x 
+echo "
+Run these small programs in coordination to take advantage of them
+ex : bash 1_varibal.sh
+And so on, in order ⬇
+- 1_varibal.sh
+- 2_read.sh
+- 3_user.sh
+- 4_math.sh
+- 5_math.sh
+
+
+
+
+
+"
 
--- README.md
+++ README.md
@@ -11,3 +11,13 @@ I created this repo for the purpose of psychological education, and I hope it wi
 You can run the project by this command ( bash + filename )
 ex : bash 0_Basics_commendline_in_unix.sh
 
+Run these small programs in coordination to take advantage of them
+ex : bash 1_varibal.sh
+
+And so on, in order ⬇
+- 1_varibal.sh
+- 2_read.sh
+- 3_user.sh
+- 4_math.sh
+- 5_math.sh
+
