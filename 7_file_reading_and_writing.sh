de61513d0977c31d5e8f9c6f59d04a682486e3dd
hossam abozeid <hossamabozeidsaleh@gmail.com>
2024-05-22 05:43:50 -0400

    commit

--- 7_file_reading_and_writing.sh
+++ 7_file_reading_and_writing.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
-echo "hello " >> testfile.txti
+#!/bin/bash 
 
+# Print the word hello inside a file testfile.txt
+# (white) to file
+read username
+while
+	[[ "$username" != "" ]]
+	echo $username >> users.txt
+	read username
+done
 
+# (Read) the file line by line 
+while read line
+do
+       	echo $line
+done < users.txt
